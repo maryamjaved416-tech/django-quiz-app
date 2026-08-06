ab36f59d360ce512c59d63d0075f7bfde54cb966
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-08-06 19:52:18 +0500

    Update admin.py

--- coursequiz/admin.py
+++ coursequiz/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor, Learner
+from.models import Course, Lesson, Enrollment, Question, Choice, Submission
 
-# Models ko Admin me register karna hai
+# Inline classes
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+# Admin classes
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'order')
+    inlines = [QuestionInline]
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'lesson', 'grade')
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+
+# Models register
 admin.site.register(Course)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Enrollment)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
-admin.site.register(Instructor)
-admin.site.register(Learner)
