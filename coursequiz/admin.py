7901555c7473a0fbd4e4a81821419985507901d7
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-08-06 19:47:00 +0500

    Update admin.py

--- coursequiz/admin.py
+++ coursequiz/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Choice, Lesson, Question, Submission, Enrollment
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor, Learner
 
-admin.site.register(Choice)
+# Models ko Admin me register karna hai
+admin.site.register(Course)
 admin.site.register(Lesson)
+admin.site.register(Enrollment)
 admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(Submission)
-admin.site.register(Enrollment)  # <-- ye line add karo
+admin.site.register(Instructor)
+admin.site.register(Learner)
